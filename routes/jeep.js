60541df8b19a421cbbcb109bfb56fb120f5553db
A  Sai Ganesh <anaparthisaiganesh222@gmail.com>
2022-11-09 23:10:36 -0600

    changes

--- routes/jeep.js
+++ routes/jeep.js
@@ -2,11 +2,6 @@ var express = require('express');
 const jeep_controlers= require('../controllers/jeep');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('jeep', { title: 'Search results jeep' });
-});
-
 module.exports = router;
 router.get('/', jeep_controlers.jeep_view_all_Page );
 
