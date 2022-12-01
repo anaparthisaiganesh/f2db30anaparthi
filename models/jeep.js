5e4ae497d5cd4826a9107694059b0fd0bc87a85f
A  Sai Ganesh <anaparthisaiganesh222@gmail.com>
2022-11-30 21:12:44 -0600

    changes

--- models/jeep.js
+++ models/jeep.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const jeepSchema = mongoose.Schema({ 
- jeep_color: String, 
- jeep_cost: Number, 
- jeep_speed: Number 
+ jeep_color: {type:String}, 
+ jeep_cost: {type:Number,min:9,max:1600000},
+ jeep_speed: {type:Number,min:8}  
 }) 
  
 module.exports = mongoose.model("jeep", jeepSchema)
--- routes/jeep.js
+++ routes/jeep.js
@@ -1,17 +1,24 @@
 var express = require('express');
 const jeep_controlers= require('../controllers/jeep');
 var router = express.Router();
-
-
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 module.exports = router;
 router.get('/', jeep_controlers.jeep_view_all_Page );
 /* GET detail jeep page */
 router.get('/detail', jeep_controlers.jeep_view_one_Page);
 /* GET create jeep page */
-router.get('/create', jeep_controlers.jeep_create_Page);
+router.get('/create',secured, jeep_controlers.jeep_create_Page);
 /* GET create update page */
-router.get('/update', jeep_controlers.jeep_update_Page);
+router.get('/update',secured, jeep_controlers.jeep_update_Page);
 /* GET delete jeep page */
-router.get('/delete', jeep_controlers.jeep_delete_Page);
+router.get('/delete',secured, jeep_controlers.jeep_delete_Page);
 
 
